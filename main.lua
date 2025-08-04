d9d3997bf9af4111ec04d2aa8819ec4c1c076174
Honi <moonhrtt@gmail.com>
2025-08-03 21:54:45 -0500

    lots of modellin stuff
    model got exported, however the models legs clip through the floor, somethings wrong with that but idunno what

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ if not _G.charSelectExists then
     return 0
 end
 
---local E_MODEL_SYBIL = smlua_model_util_get_id("sybil_geo")   -- Located in "actors"
+local E_MODEL_SYBIL = smlua_model_util_get_id("sybil_geo")   -- Located in "actors"
 --local TEX_ICON_SYBIL = get_texture_info("sybil-icon")
 
 --[[local PALETTE_SYBIL = {
@@ -29,11 +29,23 @@ end
 CHAR_SYBIL = _G.charSelect.character_add(
     "Sybil", -- Character Name
     "Cat-Bunny-goat-lady, she will slide cancel u", -- Description
-    "Honi", -- Credits
+    "Honi, Squishy", -- Credits
     "e3e643",           -- Menu Color
-    E_MODEL_MARIO,       -- Character Model
+    E_MODEL_SYBIL,       -- Character Model
     CT_MARIO,           -- Override Character
     TEX_CHAR_LIFE_ICON, -- Life Icon
     1,                  -- Camera Scale
     0                   -- Vertical Offset
 )
+
+local offset = 0
+function offset_fixer(m)
+   offset = m.marioObj.header.gfx.pos.x
+   if _G.charSelectExists then
+      if _G.charSelect.character_get_current_number() == CHAR_SYBIL then
+          offset = offset + 0x12000000
+      end
+   end
+end
+
+hook_event(HOOK_MARIO_UPDATE, offset_fixer)
